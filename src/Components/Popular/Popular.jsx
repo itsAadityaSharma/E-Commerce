7f21c3c1edc352929ce89afa6a1db25fe0e2de6c
Aaditya Sharma <saaditya940@gmail.com>
2024-01-14 14:17:02 +0530

    updateFile

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -8,10 +8,12 @@ const Popular = () => {
     <div className='popular'>
       <h1>POPULAR IN WOMEN</h1>
       <hr />
-      <div className="polular-item">
+      <div className="popular-item">
         {data_product.map((item,i)=>{
           return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-        })}
+        }
+          
+        )}
       </div>
     </div>
   )
